3bf7797284eb043a8e36e48b030656e0462da04d
Neel Redkar <neel.redkar@outlook.com>
2019-11-24 23:32:30 -0800

    add logging

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.post("/events", (req, res) => {
     if (req.body.event.channel != "C0P5NE354") { res.end() }
 
     if (req.body.event.type == "message" && req.body.event.text) {
-      console.log(req.body.event)
       let {ts, text, user, channel,thread_ts} = req.body.event;
       ts = thread_ts ? thread_ts : ts;
       var emojis = text.match(regex);
@@ -21,11 +20,16 @@ app.post("/events", (req, res) => {
           isIn(emoji,user)
             .then((is) => {
               if (is) {
+                console.log("Emoji Violator Found")
                 send(process.env.LOGS,`<@${user}> has used an emoji in a message the wrong way! The message was \`${text}\` in channel <#${channel}>`)
                 send(channel,"This message has been removed for using a restricted emoji!",ts)
+                console.log("Sent Responses")
                   .then(() => {
                     send(user,`Your message \`${text}\` was taken down in violation of using the restricted emoji ${emoji}!` )
-                    del(req.body.event.ts,channel);
+                    del(req.body.event.ts,channel)
+                      .then(() => {
+                        console.log("Deleted the message")
+                      })
                   })
               }
             })
@@ -36,8 +40,10 @@ app.post("/events", (req, res) => {
       isIn(`:${reaction}:`,user)
         .then((is) => {
           if (is) {
+            console.log("Emoji Reaction Violator Found")
             send(process.env.LOGS,`<@${user}> has used an emoji in a reaction the wrong way! The emoji was :${reaction}: in channel <#${req.body.event.item.channel}>`)
             send(user,`A reaction you posted has had a restricted emoji. The admin's will be contacted. The emoji you used was :${reaction}:!`);
+            console.log("Sent Warnings")
           }
         })
 	}
