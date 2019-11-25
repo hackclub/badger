ed2ef2946f285320289d8c4384f4a3db039dd43c
Glitch (badger) <none>
2019-11-25 06:54:08 +0000

    Add challenge

--- server.js
+++ server.js
@@ -8,38 +8,42 @@ app.get("/", (req,res) => {
 });
 
 app.post("/events", (req, res) => {
-	if (req.body.event.channel != "C0P5NE354") { res.end() }
+  try {
+    if (req.body.event.channel != "C0P5NE354") { res.end() }
 
-	if (req.body.event.type == "message" && req.body.event.text) {
-		let {ts, text, user, channel} = req.body.event;
-		var emojis = text.match(regex);
-		if (emojis) {
-			emojis.map((emoji) => {
-				isIn(emoji,user)
-					.then((is) => {
-						if (is) {
-							send(process.env.LOGS,`<@${user}> has used an emoji in a message the wrong way! The message was \`${text}\` in channel <#${channel}>`)
-							send(channel,"This message has been removed for using a restricted emoji!",ts)
-								.then(() => {
-									send(user,`Your message \`${text}\` was taken down in violation of using the restricted emoji ${emoji}!` )
-									del(ts,channel);
-								})
-						}
-					})
-			})
-		} 
-	} else if (req.body.event.type == "reaction_added"){
-		console.log(req.body.event)
-		let {user, reaction} = req.body.event;
-		isIn(`:${reaction}:`,user)
-			.then((is) => {
-				if (is) {
-					send(process.env.LOGS,`<@${user}> has used an emoji in a reaction the wrong way! The emoji was :${reaction}: in channel <#${req.body.event.item.channel}>`)
-					send(user,`A reaction you posted has had a restricted emoji. The admin's will be contacted. The emoji you used was :${reaction}:!`);
-				}
-			})
+    if (req.body.event.type == "message" && req.body.event.text) {
+      console.log(req.body.event)
+      let {ts, text, user, channel,thread_ts} = req.body.event;
+      ts = thread_ts ? thread_ts : ts;
+      var emojis = text.match(regex);
+      if (emojis) {
+        emojis.map((emoji) => {
+          isIn(emoji,user)
+            .then((is) => {
+              if (is) {
+                send(process.env.LOGS,`<@${user}> has used an emoji in a message the wrong way! The message was \`${text}\` in channel <#${channel}>`)
+                send(channel,"This message has been removed for using a restricted emoji!",ts)
+                  .then(() => {
+                    send(user,`Your message \`${text}\` was taken down in violation of using the restricted emoji ${emoji}!` )
+                    del(req.body.event.ts,channel);
+                  })
+              }
+            })
+        })
+      } 
+    } else if (req.body.event.type == "reaction_added"){
+      let {user, reaction} = req.body.event;
+      isIn(`:${reaction}:`,user)
+        .then((is) => {
+          if (is) {
+            send(process.env.LOGS,`<@${user}> has used an emoji in a reaction the wrong way! The emoji was :${reaction}: in channel <#${req.body.event.item.channel}>`)
+            send(user,`A reaction you posted has had a restricted emoji. The admin's will be contacted. The emoji you used was :${reaction}:!`);
+          }
+        })
 	}
-	res.send(req.body.challenge)
+  } finally {
+	  res.send(req.body.challenge)
+  }
 });
 
 
