e89fb3963047611c28e5ce4be4cac0559be51419
Echo <github@3kh0.net>
2026-03-06 16:46:37 -0500

    hrm fix it?

--- api/emoji.js
+++ api/emoji.js
@@ -3,12 +3,13 @@ import { getEmoji } from '../utils.js'
 export default {
   async fetch(request) {
     try {
-      const { emoji } = await getEmoji()
-      console.log(`Got ${Object.keys(emoji || {}).length} emoji`)
+      const data = await getEmoji()
+      const emoji = data.emoji || {}
+      console.log(`Got ${Object.keys(emoji).length} emoji`)
       return Response.json(emoji)
     } catch (error) {
       console.error(error)
-      return Response.json({ error: String(error) }, { status: 500 })
+      return Response.json({ error: error.message }, { status: 500 })
     }
   }
 }
